19633ce277a078948c1b52bdbb8005ed59d1b4bc
Kristie Nguyen <nguyenkristie@live.com>
2015-02-28 14:23:52 -0800

    added a bootstrap template

--- PHP/contact-form.php
+++ PHP/contact-form.php
@@ -9,12 +9,21 @@
 	 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	 	<meta name="viewport" content="width=device-width, initial-	scale=1" />
 
+	 	<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	
 	</head>
 
 	<body>
 
 		<div>
-
+			<!-- contact form that emails -->
 			<form method="post">
 				
 
