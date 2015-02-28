741c2a8fc160c8d9e479ea94eeae486db463c292
Kristie Nguyen <nguyenkristie@live.com>
2015-02-28 09:49:00 -0800

    created arrays

--- PHP/index.php
+++ PHP/index.php
@@ -1,15 +1,43 @@
 <?php
 
 	echo "This is PHP!";
+	echo "<br />";
 
 	$test = "This is a variable";
-
 	$test2 = "This is another variable";
-
 	echo $test.$test2;
+	echo "<br />";
 
 	$name = "Kristie";
-
 	echo "My name is $name";
+	echo "<br /><br />";
+
+	$myArray = array("pizza", "chocolate", "coffee");
+	print_r($myArray);
+	echo "<br />";
+
+	echo $myArray[1];
+	echo "<br /><br />";
+
+	$anotherArray[0] = "pizza";
+	$anotherArray[1] = "yogurt";
+	print_r($anotherArray);
+	echo "<br /><br />";
+
+	$thirdArray = array
+	(
+		"France" => "French",
+		"USA" => "English",
+		"Germany" => "German"
+	);
+	print_r($thirdArray);
+	echo "<br /><br />";
+
+	$anotherArray[] = "salad";
+	print_r($anotherArray);
+	echo "<br /><br />";
+
+	unset($thirdArray["Germany"]);
+	print_r($thirdArray);
 
  ?>
