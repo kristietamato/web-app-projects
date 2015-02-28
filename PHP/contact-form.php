901c80b9e07f5729a7360c2006f6c4d3c76f56f2
Kristie Nguyen <nguyenkristie@live.com>
2015-02-28 12:44:13 -0800

    created a contact form mini project

---
+++ PHP/contact-form.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+
+	<head>
+
+		<title>Creating a Contact Form with PHP</title>
+
+		<meta charset="utf-8" />
+	 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	 	<meta name="viewport" content="width=device-width, initial-	scale=1" />
+
+	</head>
+
+	<body>
+
+		<div>
+
+			<form method="post">
+				
+
+			</form>
+
+		</div>
+
+	</body>
+
+		<?php
+
+
+		?>
+
+</html>
