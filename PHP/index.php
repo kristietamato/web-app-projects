981a98429ca326c9f7e7b4895ab18933ed1a8928
Kristie Nguyen <nguyenkristie@live.com>
2015-02-28 10:39:57 -0800

    added html template to test PHP codes

--- PHP/index.php
+++ PHP/index.php
@@ -1,123 +1,28 @@
-<?php
+<!doctype html>
+<html>
 
-	echo "This is PHP!";
-	echo "<br />";
+	<head>
 
-	$test = "This is a variable";
-	$test2 = "This is another variable";
-	echo $test.$test2;
-	echo "<br />";
+		<title>Learning PHP</title>
 
-	$name = "Kristie";
-	echo "My name is $name";
-	echo "<br /><br />";
+		<meta charset="utf-8" />
+	 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	 	<meta name="viewport" content="width=device-width, initial-	scale=1" />
 
-	$myArray = array("pizza", "chocolate", "coffee");
-	print_r($myArray);
-	echo "<br />";
+	</head>
 
-	echo $myArray[1];
-	echo "<br /><br />";
+	<body>
+		<div>
+			
+		<?php
 
-	$anotherArray[0] = "pizza";
-	$anotherArray[1] = "yogurt";
-	print_r($anotherArray);
-	echo "<br /><br />";
-
-	$thirdArray = array
-	(
-		"France" => "French",
-		"USA" => "English",
-		"Germany" => "German"
-	);
-	print_r($thirdArray);
-	echo "<br /><br />";
-
-	$anotherArray[] = "salad";
-	print_r($anotherArray);
-	echo "<br /><br />";
-
-	unset($thirdArray["Germany"]);
-	print_r($thirdArray);
-	echo "<br /><br />";
+			
 
 
-	for ($i = 1; $i < 10; $i++)
-	{ 
-		echo $i."<br />";
-	}
-	echo "<br />";
+		?>
 
-	for ($i = 500; $i >= 100; $i = $i - 5)
-	{ 
-		echo $i." ";
-	}
-	echo "<br /><br />";
+		</div>
 
+	</body>
 
-	$array = array("cat", "dog", "turtle", "kangaroo");
-
-	/* foreach loop to loop through array */
-	foreach ($array as $key => $value)
-	{
-		echo "Key: $key Value: $value <br />";
-	}
-	echo "<br /><br />";
-
-
-	/* while loop tests */
-
-	$i = 0;
-
-	while ($array[$i])
-	{
-		echo "Key: $i Value: $array[$i] <br />";
-		$i++;
-	}
-	echo "<br /><br />";
-
-	while ($i < 20)
-	{
-		echo $i." ";
-		$i++;
-	}
-	echo "<br /><br />";
-
-
-	/* if/else test */
-
-	$number = 1;
-	$number2 = 2;
-	$number3 = 2;
-
-	if ($number == $number2)
-	{
-		echo "True";
-	}
-	else
-	{
-		echo "False";
-	}
-	echo "<br />";
-
-		if ($number2 == $number3)
-	{
-		echo "True";
-	}
-	else
-	{
-		echo "False";
-	}
-	echo "<br />";
-
-	if ($number == $number2 AND $number2 == $number3)
-	{
-		echo "True";
-	}
-	else
-	{
-		echo "False";
-	}
-	echo "<br />";
-
- ?>
+</html>
